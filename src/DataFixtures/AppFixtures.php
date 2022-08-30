0faedca0612ee2b31d9affe7bb551ca3392c9012
Julie J <jeannet.julie@gmail.com>
2022-08-30 20:54:14 +0200

    Installation et paramétrage des Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,50 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use Faker\Generator;
+use App\Entity\Users;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct(UserPasswordHasherInterface $passwordEncoder)
+    {
+        $this->faker = Factory::create('fr_FR');
+        $this->passwordEncoder=$passwordEncoder;
+    }
+    
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $admin = new Users();
+        $admin->setFirstName('Julie')
+            ->setLastName('Jeannet')
+            ->SetEmail('jeannet.julie@gmail.com')
+            ->setRoles(['ROLE_USER', 'ROLE_ADMIN'])
+            ->setPassword($this->passwordEncoder->hashPassword($admin, 'admin'))
+            ->setTel('0667001438');
+
+        $users[] = $admin;
+        $manager->persist($admin);
+        
+        for ($i=1; $i <= 50; $i++) { 
+            $user = new Users();
+            $user->setFirstName($this->faker->firstName())
+            ->setLastName($this->faker->lastName())
+            ->setEmail($this->faker->email())
+            ->setRoles(['ROLE_USER'])
+            ->setPassword($this->passwordEncoder->hashPassword($user, 'password'))
+            ->setTel($this->faker->phoneNumber());
+
+            $users[] = $user;
+            $manager->persist($user);
+        }
+
 
         $manager->flush();
     }
